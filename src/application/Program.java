0135131b22ac4c73fb6d670a15254755b04c5975
Yan Carvalho <yansilva303@gmail.com>
2023-12-28 19:31:23 -0300

    Final version

---
+++ src/application/Program.java
@@ -0,0 +1,48 @@
+package application;
+
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+import model.entities.Account;
+import model.exceptions.DomainExceptions;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner scan = new Scanner(System.in);
+		Boolean invalidInput = false;
+		do {
+			try {
+				System.out.println("Enter account data");
+
+				System.out.print("Number: ");
+				int number = scan.nextInt();
+				scan.nextLine();
+				System.out.print("Holder: ");
+				String holder = scan.nextLine();
+				System.out.print("Initial balance: ");
+				Double balance = scan.nextDouble();
+				System.out.print("Withdraw limit: ");
+				Double withdrawLimit = scan.nextDouble();
+
+				Account acc = new Account(number, holder, balance, withdrawLimit);
+
+				System.out.print("Enter the amount for withdraw: ");
+				Double amount = scan.nextDouble();
+				acc.withdraw(amount);
+				System.out.println(acc);
+
+				invalidInput = true;
+			} catch (InputMismatchException e) {
+				System.out.println("Invalid input type");
+				scan.nextLine();
+			} catch (DomainExceptions e) {
+				System.out.println(e.getMessage());
+			}
+		} while (!invalidInput);
+		scan.close();
+	}
+
+}
---
+++ src/model/entities/Account.java
@@ -0,0 +1,68 @@
+package model.entities;
+
+import model.exceptions.DomainExceptions;
+
+public class Account {
+
+	private Integer number;
+	private String holder;
+	private Double balance;
+	private Double withdrawLimit;
+	
+	public Account() {
+	
+	}
+
+	public Account(Integer number, String holder, Double balance, Double withdrawLimit) throws DomainExceptions{
+		this.number = number;
+		this.holder = holder;
+		this.balance = balance;
+		this.withdrawLimit = withdrawLimit;
+	}
+
+	public String getHolder() {
+		return holder;
+	}
+
+	public void setHolder(String holder) {
+		this.holder = holder;
+	}
+
+	public Double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(Double balance) {
+		this.balance = balance;
+	}
+
+	public Double getWithdrawLimit() {
+		return withdrawLimit;
+	}
+
+	public void setWithdrawLimit(Double withdrawLimit) {
+		this.withdrawLimit = withdrawLimit;
+	}
+
+	public Integer getNumber() {
+		return number;
+	}
+	
+	public void deposit(Double amount) {
+		
+	}
+	
+	public void withdraw(Double amount) throws DomainExceptions{
+		if (amount > withdrawLimit) {
+			throw new DomainExceptions("Withdraw error: the amount exceeds withdraw limit");
+		}
+			balance -= amount;	
+		}
+	
+	public String toString() {
+		return "New balance: "+String.format("%.2f", balance);
+	
+	}
+	
+	
+}
